c37d7702258f6d24c62ff552b2ee456783ae583a
Lane Bretschneider <lanebret85@gmail.com>
2023-06-22 23:59:32 -0700

    add array methods with descriptions

--- arrays.rb
+++ arrays.rb
@@ -6,3 +6,22 @@ running_distances = [3.1, 6.2, 13.1, 26.2]
 
 true_or_false = [true, true, false, false]
 
+# .pop will remove the last element from the array and return it
+fruits.pop
+
+# .push will push the added element(s) to the end of the array and return the new array
+dollar_amounts.push(50, 100)
+
+# .shift will remove the first element from the beginning of the array and return the new array
+running_distances.shift
+
+# .unshift will add elements to the beginning of the array
+true_or_false.unshift(false)
+
+puts fruits
+
+puts dollar_amounts
+
+puts running_distances
+
+puts true_or_false
